05445871ef62cd7643554e77ae6fd0a66caa6a4f
pawelhop <95289772+pawelhop@users.noreply.github.com>
2022-12-05 11:02:45 +0000

    fix deployment

--- deploy-rsync.sh
+++ deploy-rsync.sh
@@ -11,7 +11,7 @@ set -e
 # Set repo based on current branch, by default master=production, develop=staging
 # @todo support custom branches
 
-if [ "$CI_BRANCH" == "master" || "main" ]
+if [ "$CI_BRANCH" == "main" ]
 then
     target_install=${PROD_INSTALL_IP}
 else
