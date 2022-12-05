f634885f563256bd9964201f9a5cae4f50af1939
pawelhop <95289772+pawelhop@users.noreply.github.com>
2022-12-05 11:15:26 +0000

    change branch for main

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ set -e
 : ${REPO_NAME?"REPO_NAME Missing"}       # theme repo name (Typically the folder name of the project)
 : ${PROJECT_TYPE?"PROJECT_TYPE Missing"} # Whether to push theme or plugins
 
-# Set repo based on current branch, by default master=production, develop=staging
+# Set repo based on current branch, by default main=production, develop=staging
 # @todo support custom branches
 
 if [ "$CI_BRANCH" == "main" ]
@@ -35,7 +35,7 @@ cd ~/clone
 # Get official list of files/folders that are not meant to be on production if $EXCLUDE_LIST is not set.
 if [[ -z "${EXCLUDE_LIST}" ]];
 then
-    wget https://raw.githubusercontent.com/HOP-Software/sage-roots-10-wpengine-codeship-CD-CI/master/exclude-list.txt
+    wget https://raw.githubusercontent.com/HOP-Software/sage-roots-10-wpengine-codeship-CD-CI/main/exclude-list.txt
 else
     # @todo validate proper url?
     wget ${EXCLUDE_LIST}
@@ -83,7 +83,7 @@ fi
 
 # Move the gitignore file to the deployments folder
 cd ~/deployment
-wget --output-document=.gitignore https://raw.githubusercontent.com/HOP-Software/sage-roots-10-wpengine-codeship-CD-CI/master/gitignore-template.txt
+wget --output-document=.gitignore https://raw.githubusercontent.com/HOP-Software/sage-roots-10-wpengine-codeship-CD-CI/main/gitignore-template.txt
 
 # Delete plugins and theme if it exists, and move cleaned version into deployment folder
 if [ "$PROJECT_TYPE" == "theme" ]
