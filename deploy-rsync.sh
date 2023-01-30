c15790d133f6ef6da8fe2cf68a7aa6f85cc6e055
pawelhop <95289772+pawelhop@users.noreply.github.com>
2023-01-30 13:56:08 +0000

    add master  branch to script

--- deploy-rsync.sh
+++ deploy-rsync.sh
@@ -11,7 +11,7 @@ set -e
 # Set repo based on current branch, by default main=production, develop=staging
 # @todo support custom branches
 
-if [ "$CI_BRANCH" == "main" ]
+if [ "$CI_BRANCH" == "master" && "main" ]
 then
     target_install=${PROD_INSTALL_IP}
 else
